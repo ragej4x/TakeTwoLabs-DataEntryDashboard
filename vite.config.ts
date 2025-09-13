1f2b62a5f38a8a3edc5b39b285d45d94776b960c
RAGEJAX <109853879+ragej4x@users.noreply.github.com>
2025-09-13 22:09:31 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,10 @@
   import { defineConfig } from 'vite';
   import react from '@vitejs/plugin-react-swc';
   import path from 'path';
-
+  
   export default defineConfig({
     plugins: [react()],
+    base:process.env.VITE_BASE_PATH || "/TakeTwoLabs-DataEntryDashboard",
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
       alias: {
