b882196607881c3bdf94c04fae3033b4618ab9b4
RAGEJAX <109853879+ragej4x@users.noreply.github.com>
2025-09-25 01:28:28 +0800

    Add files via upload

---
+++ src/supabase.ts
@@ -0,0 +1,15 @@
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
+if (!supabaseUrl || !supabaseKey) {
+  throw new Error("Supabase environment variables are missing!");
+}
+
+export const supabase = createClient(supabaseUrl, supabaseKey);
+
+
+console.log("SUPABASE_URL:", import.meta.env.VITE_SUPABASE_URL);
+console.log("SUPABASE_KEY:", import.meta.env.VITE_SUPABASE_ANON_KEY);
+console.log(import.meta.env.VITE_TEST_VAR);
